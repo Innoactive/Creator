0bfc91c24cb9f945d508559d0c4b06107edb218a
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2020-01-27 17:46:41 +0100

    changes requested

--- Tests/SceneObjects/Properties/TrainingPropertyTests.cs
+++ Tests/SceneObjects/Properties/TrainingPropertyTests.cs
@@ -47,49 +47,61 @@ namespace Innoactive.Hub.Unity.Tests.Training
         [UnityTest]
         public IEnumerator AddTrainingProperties()
         {
+            // Given a ISceneObject.
+
             // Required for ColliderWithTriggerProperty
             SceneObject.GameObject.AddComponent<BoxCollider>().isTrigger = true;
 
             foreach (Type propertyType in TrainingProperties)
             {
+                // When adding the ISceneObjectProperty to the ISceneObject.
                 SceneObject.AddTrainingProperty(propertyType);
 
                 yield return null;
 
+                // Then assert that the ISceneObjectProperty is part of ISceneObject.
                 Assert.That(SceneObject.GameObject.GetComponent(propertyType));
             }
 
             int totalOfPublicProperties = TrainingProperties.Count();
             int totalOfAddedProperties = SceneObject.Properties.Count;
 
+            // Then assert that the ISceneObject.Properties considers all the ISceneObjectProperty added to ISceneObject.
             Assert.AreEqual(totalOfAddedProperties, totalOfPublicProperties);
         }
 
         [UnityTest]
         public IEnumerator AddAndRemoveTrainingProperties()
         {
+            // Given a ISceneObject.
+
             // Required for ColliderWithTriggerProperty
             SceneObject.GameObject.AddComponent<BoxCollider>().isTrigger = true;
 
+            // When adding a list of ISceneObjectProperty to the ISceneObject.
             yield return AddTrainingProperties();
 
             foreach (Component propertyComponent in SceneObject.GameObject.GetComponents(typeof(TrainingSceneObjectProperty)))
             {
+                // When removing a ISceneObjectProperty from the ISceneObject.
                 SceneObject.RemoveTrainingProperty(propertyComponent);
 
                 yield return null;
 
+                // Then assert that the ISceneObjectProperty is no longer part of ISceneObject.
                 Assert.That(SceneObject.GameObject.GetComponent(propertyComponent.GetType()) == null);
             }
 
             int totalOfAddedProperties = SceneObject.Properties.Count;
 
+            // Then assert that all ISceneObjectProperty were removed.
             Assert.AreEqual(0, totalOfAddedProperties);
         }
 
         [UnityTest]
         public IEnumerator AddPropertyWithDependencies()
         {
+            // Given an ISceneObjectProperty and a list with its dependencies.
             List<Type> dependencies = new List<Type>();
             Type trainingProperty = TrainingProperties.First(propertyType => GetAllDependenciesFrom(propertyType, ref dependencies));
 
@@ -99,12 +111,15 @@ namespace Innoactive.Hub.Unity.Tests.Training
                 Assert.Ignore();
             }
 
+            // When adding the ISceneObjectProperty.
+            // Then assert that all its dependencies were added.
             yield return AddPropertyAndVerifyDependencies(trainingProperty, dependencies);
         }
 
         [UnityTest]
         public IEnumerator RemovePropertyAndDependencies()
         {
+            // Given an ISceneObjectProperty and a list with its dependencies.
             List<Type> dependencies = new List<Type>();
             Type trainingProperty = TrainingProperties.First(propertyType => GetAllDependenciesFrom(propertyType, ref dependencies));
 
@@ -114,16 +129,20 @@ namespace Innoactive.Hub.Unity.Tests.Training
                 Assert.Ignore();
             }
 
+            // When adding the ISceneObjectProperty, we also make sure that all its dependencies were added.
             yield return AddPropertyAndVerifyDependencies(trainingProperty, dependencies);
 
+            // When removing the ISceneObjectProperty forcing to remove its dependencies.
             SceneObject.RemoveTrainingProperty(trainingProperty, true);
 
             yield return null;
 
+            // Then assert that the ISceneObjectProperty is not longer part of ISceneObject.
             Assert.That(SceneObject.GameObject.GetComponent(trainingProperty) == null);
 
             foreach (Type dependency in dependencies)
             {
+                // Then assert that the dependencies of the ISceneObjectProperty are not longer part of ISceneObject.
                 Assert.That(SceneObject.GameObject.GetComponent(dependency) == null);
             }
         }
@@ -131,6 +150,7 @@ namespace Innoactive.Hub.Unity.Tests.Training
         [UnityTest]
         public IEnumerator AddPropertyAndRemoveDependency()
         {
+            // Given an ISceneObjectProperty, a list with its dependencies and a type dependant of ISceneObjectProperty.
             List<Type> dependencies = new List<Type>();
             Type trainingProperty = TrainingProperties.First(propertyType => GetAllDependenciesFrom(propertyType, ref dependencies, true)), dependantType = dependencies.First();
 
@@ -140,17 +160,19 @@ namespace Innoactive.Hub.Unity.Tests.Training
                 Assert.Ignore();
             }
 
-            SceneObject.AddTrainingProperty(trainingProperty);
+            // When adding adding the ISceneObjectProperty, we also make sure that all its dependencies were added.
+            yield return AddPropertyAndVerifyDependencies(trainingProperty, dependencies);
 
-            yield return null;
-
+            // Then assert that the ISceneObjectProperty and the property that depends of it are part of ISceneObject.
             Assert.That(SceneObject.GameObject.GetComponent(trainingProperty));
             Assert.That(SceneObject.GameObject.GetComponent(dependantType));
 
+            // When removing the property that depends of ISceneObjectProperty.
             SceneObject.RemoveTrainingProperty(dependantType);
 
             yield return null;
 
+            // Then assert that the ISceneObjectProperty and the property that depends of it are no longer part of ISceneObject.
             Assert.That(SceneObject.GameObject.GetComponent(trainingProperty) == null);
             Assert.That(SceneObject.GameObject.GetComponent(dependantType) == null);
         }
@@ -158,6 +180,7 @@ namespace Innoactive.Hub.Unity.Tests.Training
         [UnityTest]
         public IEnumerator RemovePropertyWithoutDependencies()
         {
+            // Given an ISceneObjectProperty and a list with its dependencies.
             List<Type> dependencies = new List<Type>();
             Type trainingProperty = TrainingProperties.First(propertyType => GetAllDependenciesFrom(propertyType, ref dependencies));
 
@@ -167,17 +190,21 @@ namespace Innoactive.Hub.Unity.Tests.Training
                 Assert.Ignore();
             }
 
+            // When adding adding the ISceneObjectProperty, we also make sure that all its dependencies were added.
             yield return AddPropertyAndVerifyDependencies(trainingProperty, dependencies);
 
+            // When removing the ISceneObjectProperty without forcing to remove its dependencies.
             // Parameter removeDependencies is automatically initialized as false.
             SceneObject.RemoveTrainingProperty(trainingProperty);
 
             yield return null;
 
+            // Then assert that the ISceneObjectProperty is not longer part of ISceneObject.
             Assert.That(SceneObject.GameObject.GetComponent(trainingProperty) == null);
 
             foreach (Type dependency in dependencies)
             {
+                // Then assert that the dependencies of the ISceneObjectProperty continuing in ISceneObject.
                 Assert.That(SceneObject.GameObject.GetComponent(dependency));
             }
         }
@@ -185,26 +212,32 @@ namespace Innoactive.Hub.Unity.Tests.Training
         [UnityTest]
         public IEnumerator TryToAddNotTrainingProperties()
         {
+            // Given an ISceneObject and a list of non-ISceneObjectProperty types.
             foreach (Type notAPropertyType in NotTrainingProperties)
             {
+                // When trying to add a non-ISceneObjectProperty using ISceneObject.AddTrainingProperty.
                 SceneObject.AddTrainingProperty(notAPropertyType);
 
                 yield return null;
 
+                // Then assert that the type was not attached to ISceneObject.
                 Assert.That(SceneObject.GameObject.GetComponent(notAPropertyType) == null);
             }
         }
 
         private IEnumerator AddPropertyAndVerifyDependencies(Type trainingProperty, List<Type> dependencies)
         {
+            // Given an ISceneObject.
             SceneObject.AddTrainingProperty(trainingProperty);
 
+            // When adding an ISceneObjectProperty.
             Assert.That(SceneObject.GameObject.GetComponent(trainingProperty));
 
             yield return null;
 
             foreach (Type dependency in dependencies)
             {
+                // Then assert that ISceneObject has all the dependencies required by the ISceneObjectProperty.
                 Assert.That(SceneObject.GameObject.GetComponent(dependency));
             }
         }
