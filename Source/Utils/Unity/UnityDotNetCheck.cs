d3a8ac2137520248ea1dd8750126cd51ff8a84d3
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2020-02-05 15:58:34 +0100

    chg: .Net compatibility level is only checked in Unity versions older than 2018.3 (#17)

--- Source/Utils/Unity/UnityDotNetCheck.cs
+++ Source/Utils/Unity/UnityDotNetCheck.cs
@@ -3,10 +3,9 @@
 // succeed before running, which isn't the case for .Net subset.
 // If Scripting Runtime Version is .NET 4.X, Api Compatibility Level 4.X is required, since the
 // .Net Standard 2.0 does not provide access to Win32.Registry.
+
 #if NET_2_0_SUBSET
 #error This Unity project is configured for .Net 2.0 subset, but the Innoactive Hub SDK requires full .Net functionality. Please go to "PlayerSettings > Other Settings" and change Api Compatibility Level to .Net 4.X.
-#elif NET_STANDARD_2_0
+#elif NET_STANDARD_2_0 && !UNITY_2018_3_OR_NEWER
 #error This Unity project is configured for Scripting Runtime Version 4.X, but uses Api Compatibility for .Net Standard 2.0. The Innoactive Hub SDK requires Api Compatibility level 4.X. Please go to "PlayerSettings > Other Settings" and change Api Compatibility Level to .Net 4.X.
 #endif
-
-
