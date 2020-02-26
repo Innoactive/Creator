028c21f9e11bbc2500188b68ba0d5bd31f44372a
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2020-02-26 15:56:31 +0100

    New FileManager (#27)
    - add: new namespace Innoactive.Creator.IO with functionality for reading and writing files from Unity's streaming assets and persistent data folders.
    - chg: IRuntimeConfiguration.LoadCourse was changed from ICourse to Task<ICourse>.

---
+++ Source/FileManager/FileManager.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Innoactive.Creator.IO
+{
+    /// <summary>
+    /// Handles runtime operations that allow reading and writing to files in Unity.
+    /// </summary>
+    public static class FileManager
+    {
+        private static IPlatformFileSystem platformFileSystem;
+
+        [RuntimeInitializeOnLoadMethod]
+        private static void Initialize()
+        {
+            platformFileSystem = new DefaultFileSystem(Application.streamingAssetsPath, Application.persistentDataPath);
+        }
+
+        /// <summary>
+        /// Loads a file stored at <paramref name="filePath"/>.
+        /// Returns a `FileNotFoundException` if file does not exist.
+        /// </summary>
+        /// <param name="filePath">Path where the file is located.</param>
+        /// <param name="cacheLocation">Cache directory where to lok</param>
+        /// <remarks><paramref name="filePath"/> must be relative to the StreamingAssets or the persistent data folder.</remarks>
+        /// <returns>An asynchronous operation that returns a byte array containing the contents of the file.</returns>
+        public static async Task<byte[]> Read(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException("Invalid 'filePath'");
+            }
+
+            if (Path.IsPathRooted(filePath))
+            {
+                throw new ArgumentException($"Method only accepts relative paths.\n'filePath': {filePath}");
+            }
+
+            return await platformFileSystem.Read(filePath);
+        }
+
+        /// <summary>
+        /// Saves given <paramref name="fileData"/> in provided <paramref name="filePath"/>.
+        /// </summary>
+        /// <remarks><paramref name="filePath"/> must be relative to <see cref="PersistentDataPath"/>.</remarks>
+        /// <returns>Returns true if <paramref name="fileData"/> could be saved successfully; otherwise, false.</returns>
+        public static bool Write(string filePath, byte[] fileData)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException("Invalid 'filePath'");
+            }
+
+            if (Path.IsPathRooted(filePath))
+            {
+                throw new ArgumentException($"Method only accepts relative paths.\n'filePath': {filePath}");
+            }
+
+            if (fileData == null || fileData.Length == 0)
+            {
+                throw new ArgumentException("Invalid 'fileData'");
+            }
+
+            return platformFileSystem.Write(filePath, fileData);
+        }
+
+        /// <summary>
+        /// Returns true if given <paramref name="filePath"/> contains the name of an existing file under the StreamingAssets or platform persistent data folder; otherwise, false.
+        /// </summary>
+        /// <remarks><paramref name="filePath"/> must be relative to the StreamingAssets or the platform persistent data folder.</remarks>
+        public static bool Exists(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentException("Invalid 'filePath'");
+            }
+
+            if (Path.IsPathRooted(filePath))
+            {
+                throw new ArgumentException($"Method only accepts relative paths.\n'filePath': {filePath}");
+            }
+
+            return platformFileSystem.Exists(filePath);
+        }
+    }
+}
---
+++ Tests/IO/FileManagerTests.cs
@@ -0,0 +1,62 @@
+﻿#if UNITY_EDITOR
+
+using System.Text;
+using System.Collections;
+using System.Threading.Tasks;
+using UnityEngine;
+using NUnit.Framework;
+using UnityEngine.TestTools;
+using Innoactive.Creator.IO;
+
+namespace Innoactive.Creator.Tests
+{
+    public class FileManagerTests : IOTests
+    {
+        [UnityTest]
+        public IEnumerator Read()
+        {
+            // Given
+
+            // When
+            TaskCompletionSource<byte[]> completionSource = new TaskCompletionSource<byte[]>();
+            ReadAsync(completionSource);
+            yield return new WaitUntil(()=> completionSource.Task.IsCompleted);
+
+            // Then
+            byte[] fileData = completionSource.Task.Result;
+            string message = Encoding.Default.GetString(fileData);
+
+            Assert.That(fileData != null && fileData.Length > 0);
+            Assert.IsFalse(string.IsNullOrEmpty(message));
+        }
+
+        [UnityTest]
+        public IEnumerator Exists()
+        {
+            // Given
+            // When
+            // Then
+            Assert.IsTrue(FileManager.Exists(RelativeFilePath));
+            yield break;
+        }
+
+        [UnityTest]
+        public IEnumerator Write()
+        {
+            Assert.IsFalse(FileManager.Exists(RelativeFilePath));
+
+            // Given
+            byte[] fileData = new UTF8Encoding(true).GetBytes(FileContent);
+
+            // When
+            FileManager.Write(RelativeFilePath, fileData);
+
+
+            // Then
+            Assert.IsTrue(FileManager.Exists(RelativeFilePath));
+            yield break;
+        }
+    }
+}
+
+#endif
