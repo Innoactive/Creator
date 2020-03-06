fba80a9d2b19a65e6cd8149688b52d200849a883
Thomas W <tomwim@users.noreply.github.com>
2020-03-06 11:45:30 +0100

    Remove VRTK from Scene Setup and introduce IOnSceneSetup (#32)
    * Remove VRTK from Scene Setup and introduce OnSceneSetup

---
+++ Source/Utils/SceneSetup/TrainingSceneSetup.cs
@@ -0,0 +1,16 @@
+﻿using Innoactive.Hub.Training.Unity.Utils;
+
+namespace Innoactive.Creator.Utils
+{
+    /// <summary>
+    /// Scene setup for training configuration.
+    /// </summary>
+    public class TrainingSceneSetup : OnSceneSetup
+    {
+        /// <inheritdoc />
+        public override void Setup()
+        {
+            SceneUtils.SetupTrainingConfiguration();
+        }
+    }
+}
