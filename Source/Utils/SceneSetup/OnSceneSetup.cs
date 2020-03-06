b079c970753daa99ba27ea5eaff0708560cbe091
Tom Wim <samo.miw@gmail.com>
2020-03-06 10:03:41 +0100

    Remove VRTK from Scene Setup and introduce IOnSceneSetup

---
+++ Source/Utils/SceneSetup/OnSceneSetup.cs
@@ -0,0 +1,17 @@
+﻿namespace Innoactive.Creator.Utils
+{
+    /// <summary>
+    /// This interface is supposed to be implemented by classes which will be called to setup the scene.
+    /// Can be used to e.g. setup training classes or interaction frameworks.
+    /// </summary>
+    /// <remarks>
+    /// See <see cref="TrainingSceneSetup"/> as a reference.
+    /// </remarks>
+    public abstract class OnSceneSetup
+    {
+        /// <summary>
+        /// Setup the scene with necessary objects and/or logic.
+        /// </summary>
+        public abstract void Setup();
+    }
+}
