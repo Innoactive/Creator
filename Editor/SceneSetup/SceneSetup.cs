8fbde20142de167446b2aaa5c6cf54474559a4b1
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2020-03-26 20:32:06 +0100

    Priority factor added to SceneSetup (#41)
    add: Priority factor for setting up scenes.
    chg: A validation for avoiding test properties was added to the property manager.
    chg: Rename OnSceneSetup to SceneSetup

--- Editor/SceneSetup/SceneSetup.cs
+++ Editor/SceneSetup/SceneSetup.cs
@@ -7,13 +7,19 @@
     /// <remarks>
     /// See <see cref="TrainingSceneSetup"/> as a reference.
     /// </remarks>
-    public abstract class OnSceneSetup
+    public abstract class SceneSetup
     {
         /// <summary>
         /// Identifier key for specific scene setup types,
         /// e.g. for every interaction framework.
         /// </summary>
         public virtual string Key { get; } = null;
+
+        /// <summary>
+        /// Priority lets you tweak in which order different <see cref="SceneSetup"/>s will be performed.
+        /// The priority is considered from lowest to highest.
+        /// </summary>
+        public virtual int Priority { get; } = 0;
 
         /// <summary>
         /// Setup the scene with necessary objects and/or logic.
