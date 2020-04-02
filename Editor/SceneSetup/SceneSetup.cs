25ffc0c5458f5712062f73a4d75d1ee961fe77fa
Gustavo Quiroz <gusinuhe@gmail.com>
2020-04-02 22:54:44 +0200

    add: Secure prefab instantiation methods in SceneSetup.

--- Editor/SceneSetup/SceneSetup.cs
+++ Editor/SceneSetup/SceneSetup.cs
@@ -1,4 +1,13 @@
-﻿namespace Innoactive.CreatorEditor
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using CommandLine;
+using UnityEditor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Innoactive.CreatorEditor
 {
     /// <summary>
     /// This base class is supposed to be implemented by classes which will be called to setup the scene.
@@ -25,5 +34,43 @@
         /// Setup the scene with necessary objects and/or logic.
         /// </summary>
         public abstract void Setup();
+
+        /// <summary>
+        /// Sets up given <paramref name="prefab"/> in current scene.
+        /// </summary>
+        protected void SetupPrefab(string prefab)
+        {
+            string prefabName = Path.GetFileName(prefab);
+
+            if (IsPrefabMissingInScene(prefabName))
+            {
+                string filter = $"{prefab} t:Prefab";
+                string[] prefabsGUIDs = AssetDatabase.FindAssets(filter, null);
+
+                if (prefabsGUIDs.Any() == false)
+                {
+                    throw new FileNotFoundException($"No prefabs found that match \"{prefab}\"." );
+                }
+
+                string assetPath = AssetDatabase.GUIDToAssetPath(prefabsGUIDs.First());
+                AssetDatabase.ImportAsset(assetPath);
+
+                string[] brokenPaths = Regex.Split(assetPath, "Resources/");
+                string relativePath = brokenPaths.Last().Replace(".prefab", string.Empty);
+
+                GameObject instance = Object.Instantiate(Resources.Load(relativePath, typeof(GameObject))) as GameObject;
+                instance.name = instance.name.Replace("(Clone)", string.Empty);
+            }
+        }
+
+        /// <summary>
+        /// Returns true if given <paramref name="prefabName"/> is missing in current scene.
+        /// </summary>
+        protected bool IsPrefabMissingInScene(string prefabName)
+        {
+            GameObject prefab = GameObject.Find(prefabName);
+
+            return prefab == null;
+        }
     }
 }
