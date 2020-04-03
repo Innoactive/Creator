39eec0847593229a2ed302336b1f1c215333ae9a
Simon Lerch <simon.lerch@innoactive.de>
2020-04-03 14:24:04 +0200

    added an assetdatabase refresh

--- Editor/UI/Windows/RenameCoursePopup.cs
+++ Editor/UI/Windows/RenameCoursePopup.cs
@@ -91,6 +91,7 @@ namespace Innoactive.CreatorEditor.UI.Windows
                                 SaveManager.SaveTrainingCourseToFile(course);
                                 RuntimeConfigurator.Instance.SetSelectedTrainingCourse(EditorCourseUtils.GetCoursePath(course));
                                 TrainingWindow.GetWindow().IsDirty = false;
+                                AssetDatabase.Refresh();
                             }
                         },
                         // ReSharper disable once ImplicitlyCapturedClosure
@@ -109,6 +110,7 @@ namespace Innoactive.CreatorEditor.UI.Windows
 
                             SaveManager.SaveTrainingCourseToFile(course);
                             RuntimeConfigurator.Instance.SetSelectedTrainingCourse(oldPath.Substring(Application.streamingAssetsPath.Length + 1));
+                            AssetDatabase.Refresh();
                         }
                     ));
                 }
