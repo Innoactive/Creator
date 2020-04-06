defb1955991713b0d88f20497fb8cd3cd99a9db1
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2020-04-06 15:29:34 +0200

    Dependency Manager (#54)
    add: DependencyManager
    chg: SceneSetup doesn't validate prefabs anymore.
    chg: EditorUtils don't add OpenVR anymore.
    add: Dependency manager article.

---
+++ Editor/PackageManager/Dependency.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Innoactive.CreatorEditor.PackageManager
+{
+    /// <summary>
+    /// Base class for dependencies used by the <see cref="DependencyManager"/>.
+    /// </summary>
+    public abstract class Dependency
+    {
+        /// <summary>
+        /// A string representing the package to be added.
+        /// </summary>
+        public virtual string Package { get; } = "";
+
+        /// <summary>
+        /// Priority lets you tweak in which order each <see cref="Dependency"/> will be performed.
+        /// The priority is considered from lowest to highest.
+        /// </summary>
+        public virtual int Priority { get; } = 0;
+
+        /// <summary>
+        /// Emitted when this <see cref="Dependency"/> is set as enabled.
+        /// </summary>
+        public event EventHandler<EventArgs> OnPackageEnabled;
+
+        /// <summary>
+        /// Emitted when this <see cref="Dependency"/> is set as disabled.
+        /// </summary>
+        public event EventHandler<EventArgs> OnPackageDisabled;
+
+        /// <summary>
+        /// Represents the current status of this <see cref="Dependency"/>.
+        /// </summary>
+        internal bool IsEnabled
+        {
+            get => isEnabled;
+            set
+            {
+                if (isEnabled != value)
+                {
+                    if (value)
+                    {
+                        EmitOnEnabled();
+                    }
+                    else
+                    {
+                        EmitOnDisabled();
+                    }
+                }
+
+                isEnabled = value;
+            }
+        }
+
+        private bool isEnabled;
+
+        protected virtual void EmitOnEnabled()
+        {
+            OnPackageEnabled?.Invoke(this, EventArgs.Empty);
+        }
+
+        protected virtual void EmitOnDisabled()
+        {
+            OnPackageDisabled?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
