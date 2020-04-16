23ec061c68f13d68b79babc8ccc355337e2a2dc0
Simon L <simon.lerch@innoactive.de>
2020-04-16 12:21:32 +0200

    Added basic tests to check if the training confiugration setup works (#62)

---
+++ Tests/Editor/Setup/RuntimeConfigurationSetupTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Innoactive.Creator.Core;
+using Innoactive.Creator.Core.Configuration;
+using Innoactive.Creator.Tests.Builder;
+using Innoactive.Creator.Tests.Utils.Mocks;
+using Innoactive.Creator.Unity;
+using Innoactive.CreatorEditor.Utils;
+using NUnit.Framework;
+using UnityEditor;
+using UnityEngine;
+
+namespace Innoactive.CreatorEditor.Tests
+{
+    public class RuntimeConfigurationSetupTests
+    {
+        [Test]
+        public void ConfigNotCreated()
+        {
+            // When the Runtime configuration setup is ran.
+            new RuntimeConfigurationSetup().Setup();
+            // Then there should be an GameObject with fitting name in scene.
+            GameObject obj = GameObject.Find(RuntimeConfigurationSetup.TrainingConfiugrationName);
+            Assert.NotNull(obj);
+        }
+
+        [Test]
+        public void IsConfigWithoutMissingScriptTest()
+        {
+            // When the Runtime configuration setup is ran.
+            new RuntimeConfigurationSetup().Setup();
+            // Then the found GameObject should not have missing scripts.
+            GameObject obj = GameObject.Find(RuntimeConfigurationSetup.TrainingConfiugrationName);
+            Assert.NotNull(obj);
+            Assert.False(SceneUtils.ContainsMissingScripts(obj));
+        }
+    }
+}
