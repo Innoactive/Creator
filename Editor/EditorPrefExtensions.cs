e61fde78af85c4f50371f8dc04c5611a33261ae8
Simon Lerch <simon.lerch@innoactive.de>
2020-05-08 10:40:32 +0200

    add docu

--- Editor/EditorPrefExtensions.cs
+++ Editor/EditorPrefExtensions.cs
@@ -7,7 +7,13 @@ namespace Innoactive.CreatorEditor.Analytics
 {
     internal static class EditorPrefExtensions
     {
-        public static T GetEnum<T>(string key, T defaultValue)
+        /// <summary>
+        /// Returns a value from editor preferences as enum.
+        /// </summary>
+        /// <param name="key">key of the entry</param>
+        /// <param name="defaultValue">Value which should be returned if no entry is found</param>
+        /// <typeparam name="T">Enum Type</typeparam>
+        public static T GetEnum<T>(string key, T defaultValue) where T : Enum
         {
             string value = EditorPrefs.GetString(key, null);
             if (string.IsNullOrEmpty(value))
@@ -18,7 +24,13 @@ namespace Innoactive.CreatorEditor.Analytics
             return (T)Enum.Parse(typeof(T), value);
         }
 
-        public static void SetEnum<T>(string key, T value)
+        /// <summary>
+        /// Sets an enum value as string into EditorPrefs
+        /// </summary>
+        /// <param name="key">Key of the entry</param>
+        /// <param name="value">value which should be stored</param>
+        /// <typeparam name="T">Enum Type</typeparam>
+        public static void SetEnum<T>(string key, T value) where T : Enum
         {
             EditorPrefs.SetString(key, value.ToString());
         }
