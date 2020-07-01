220cd104e4904a0f8d7ca3a52a421f83b2594419
Maxsiom <maximilian.hotter@tum.de>
2020-07-01 16:42:41 +0200

    test: add unit tests for autolocking feature

---
+++ Tests/Utils/Mocks/Properties/ILockablePropertyMock.cs
@@ -0,0 +1,12 @@
+﻿using Innoactive.Creator.Core.SceneObjects;
+using Innoactive.Creator.Core.Properties;
+
+namespace Innoactive.Creator.Tests.Utils.Mocks
+{
+    /// <summary>
+    /// Interface for a <see cref="LockablePropertyMock"/>.
+    /// </summary>
+    public interface ILockablePropertyMock : ISceneObjectProperty, ILockable
+    {
+    }
+}
