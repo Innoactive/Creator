79ff89887ce2dfce919ec34e8644b9ba3cb1b551
Simon Lerch <simon.lerch@innoactive.de>
2020-07-03 10:25:32 +0200

    refactor: added new NonLockingStepHandling and missing xml-doc

--- Runtime/RestrictiveEnvironment/StepLockHandlingStrategy.cs
+++ Runtime/RestrictiveEnvironment/StepLockHandlingStrategy.cs
@@ -1,11 +1,24 @@
 ﻿using System.Collections.Generic;
-using Innoactive.Creator.Core.RestrictiveEnvironment;
 
 namespace Innoactive.Creator.Core.RestrictiveEnvironment
 {
+    /// <summary>
+    /// Allows to implement strategies which restrict interaction with scene objects for specific steps.
+    /// </summary>
     public abstract class StepLockHandlingStrategy
     {
+        /// <summary>
+        /// Unlocks the restrictive environment and allows interaction with scene objects required for this Step to complete.
+        /// </summary>
+        /// <param name="data">IStepData of the current step</param>
+        /// <param name="manualUnlocked">All LockableProperties which are should be unlocked in addition</param>
         public abstract void Unlock(IStepData data, IEnumerable<LockablePropertyData> manualUnlocked);
+
+        /// <summary>
+        /// Locks all unlocked LockableProperties for the current step.
+        /// </summary>
+        /// <param name="data">IStepData of the current step</param>
+        /// <param name="manualUnlocked">All LockableProperties which were unlocked in addition</param>
         public abstract void Lock(IStepData data, IEnumerable<LockablePropertyData> manualUnlocked);
     }
 }
