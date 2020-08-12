5668186807eb9d98ec48466a48b188c92e117bd0
Simon Lerch <simon.lerch@innoactive.de>
2020-08-12 10:24:23 +0200

    feat(wizard): improved test

--- Editor/UI/Wizard/FirstTime/AnalyticsPage.cs
+++ Editor/UI/Wizard/FirstTime/AnalyticsPage.cs
@@ -31,13 +31,13 @@ namespace Innoactive.Creator.Core.Editor.UI.Wizard
             SetupStyles();
 
             GUILayout.BeginArea(new Rect(window.x, window.y, window.width, window.height));
-                GUILayout.Label("Help us! Contribute to Improvements!", titleStyle);
-                GUILayout.Box("Innoactive Creator is actively evolving. Please, help us with your feedback.  Provide us your <b>anonymous</b> usage data and contribute to improvements.", textStyle);
+                GUILayout.Label("Help Us! Contribute to Improvements!", titleStyle);
+                GUILayout.Box("Innoactive Creator is actively evolving. Please, help us with your feedback. Provide us your <b>anonymous</b> usage data and contribute to improvements.", textStyle);
 
-                GUILayout.Label("We Respect your Privacy", titleStyle);
+                GUILayout.Label("We Respect Your Privacy", titleStyle);
                 GUILayout.Box("We DO NOT collect any sensitive information such as source code, file names or your courses' structure.\n\nHere is what we collect:\n- exact version of Innoactive Creator\n- exact version of Unity Editor\n- your system's language\n- information about usage of the Innoactive Creator's components\n\nIn order to collect the information above, we store a unique identifier within Unity's Editor Preferences. Your data is anonymized.", textStyle);
 
-                GUILayout.Label("We are Transparent", titleStyle);
+                GUILayout.Label("We Are Transparent", titleStyle);
                 GUILayout.Box("The Innoactive Creator is open-source. Feel free to check our analytics code in <b>Core/Editor/Analytics</b>\n\nIf you want to opt-out of tracking, open <b>Innoactive > Creator > Windows > Analytics Settings</b> in the Unity's menu bar and choose <i>disabled</i> from the drop-down menu.", textStyle);
 
                 GUILayout.Space(4f);
