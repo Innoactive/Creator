a48e7916a904ad83200896dd4bd5c1dca4212498
Simon Lerch <simon.lerch@innoactive.de>
2020-11-11 11:17:35 +0100

    refactor: improved tracking information

--- Editor/Analytics/GoogleTracker.cs
+++ Editor/Analytics/GoogleTracker.cs
@@ -1,4 +1,5 @@
 ﻿using System.Net.Http;
+using UnityEngine;
 
 namespace Innoactive.CreatorEditor.Analytics
 {
@@ -8,6 +9,8 @@ namespace Innoactive.CreatorEditor.Analytics
     internal class GoogleTracker : BaseAnalyticsTracker
     {
         private readonly HttpClient client = new HttpClient();
+
+        private readonly string agent = $"Unity-{Application.unityVersion}";
 
         public override void Send(AnalyticsEvent data)
         {
@@ -28,10 +31,22 @@ namespace Innoactive.CreatorEditor.Analytics
             return string.Format(uri, data.Category, data.Action, data.Label);
         }
 
+        public override void SendSessionStart()
+        {
+            PostData(GetBaseUri() + "&sc=start");
+        }
+
         private string GetBaseUri()
         {
-            string baseUri = "https://www.google-analytics.com/collect?v=1&tid=UA-109665637-9&cid={0}&aip=1&npa=1&ul={1}&an=Creator&av={2}";
-            return string.Format(baseUri, SessionId, GetLanguage(), EditorUtils.GetCoreVersion());
+
+            string baseUri = "https://www.google-analytics.com/collect?v=1&tid=UA-109665637-9&cid={0}&aip=1&npa=1&ul={1}&an=Creator&av={2}&ua={3}&ds=unity";
+#if CREATOR_PRO
+            if (CreatorPro.Account.UserAccount.IsAccountLoggedIn())
+            {
+                baseUri += $"&uid={CreatorPro.Account.UserAccount.GetId()}";
+            }
+#endif
+            return string.Format(baseUri, SessionId, GetLanguage(), EditorUtils.GetCoreVersion(), agent);
         }
     }
 }
