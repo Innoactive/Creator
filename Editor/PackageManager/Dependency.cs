1223030126d6795a3a395fc9b7322ceab4e136af
Gustavo Quiroz <gustavo.quiroz@innoactive.de>
2021-01-15 16:38:28 +0100

    fix: ocd

--- Editor/PackageManager/Dependency.cs
+++ Editor/PackageManager/Dependency.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEditor;
 using UnityEditor.PackageManager.UI;
-using UnityEngine;
 
 namespace Innoactive.CreatorEditor.PackageManager
 {
