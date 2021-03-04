3827288b3be8c29941b3fc2f013bfe5866de1584
Simon Lerch <simon.lerch@innoactive.de>
2021-03-04 10:20:14 +0100

    feat: added course events to the CourseRunner

---
+++ Runtime/CourseEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Innoactive.Creator.Core
+{
+    public class CourseEventArgs : EventArgs
+    {
+        public readonly ICourse Course;
+        public readonly IChapter Chapter;
+        public readonly IStep CurrentStep;
+
+        public CourseEventArgs(ICourse course)
+        {
+            Course = course;
+            Chapter = course.Data.Current;
+            if (Chapter != null)
+            {
+                CurrentStep = Chapter.Data.Current;
+            }
+        }
+    }
+}
